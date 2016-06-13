417ba94bc4a142dae6a2afb2daa7b974af6ff0bf
Caleb Stewart <caleb.stewart94@gmail.com>
2016-06-13 12:16:03 -0500

    Added custom toolchain options for StewieOS within binutils

---
+++ ld/emulparams/elf_i386_stewieos.sh
@@ -0,0 +1,7 @@
+# @Author: caleb
+# @Date:   2016-05-22 08:47:42
+# @Last Modified by:   caleb
+# @Last Modified time: 2016-05-22 08:48:31
+. ${srcdir}/emulparams/elf_i386.sh
+GENERATE_SHLIB_SCRIPT=yes
+GENERATE_PIE_SCRIPT=yes
---
+++ ld/emulparams/elf_x86_64_stewieos.sh
@@ -0,0 +1,5 @@
+# @Author: caleb
+# @Date:   2016-05-22 08:50:18
+# @Last Modified by:   caleb
+# @Last Modified time: 2016-05-22 08:50:36
+. ${srcdir}/emulparams/elf_x86_64.sh
